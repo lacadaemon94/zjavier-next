d58605e6b04376a88c4fe68761843f899bfe96b6
lacadaemon94 <javier.flores@zjavier.com>
2026-04-26 22:23:10 -0600

    chore: remove old contentlayer post helper

--- src/app/utils/posts/getPublishedPosts.ts
+++
@@ -1,9 +0,0 @@
-import { allPosts, Post } from "contentlayer/generated";
-
-export function getPublishedPosts(): Post[] {
-  return allPosts.filter((post) => post.status === "published");
-}
-
-export function getPublishedPostBySlug(slug: string): Post | undefined {
-  return getPublishedPosts().find((post) => post.slug === slug);
-}
